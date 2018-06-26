2931eafaf400c37961fe85fd59a6bb2ee406bc54
Lia Hiscock <liahiscock@outlook.com>
2018-06-26 09:21:12 -0700

    made button clickable only once

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,5 @@
-function randomNum() {
+function randomNum(){
     document.getElementById("generate").innerHTML = Math.floor(Math.random()*8 + 2);
-    document.getElementById("x").innerHTML = "MASH";
-
+    document.getElementById("popupText").innerHTML = "MASH";
+    document.getElementById("generate").disabled = true;
 }
