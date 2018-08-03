88bcc10f433fee11cf5451545c875ac66de80fa9
Lia Hiscock <liahiscock@outlook.com>
2018-08-03 08:38:06 -0700

    added php logging

--- executeGoogleSearch.php
+++ executeGoogleSearch.php
@@ -5,10 +5,10 @@ $key = getenv('APIKey');
 $excludedSites = "-site:facebook.com -site:pinterest.com";
 $searchTerm = (array) json_decode(file_get_contents('php://input'));
 $url= $baseURL . "?cx=" . $cx . "&key=" . $key . "&searchType=image&q=" . $searchTerm['q'] . " " . $excludedSites;
-//$handle = curl_init();
-//curl_setopt($handle, CURLOPT_URL, $url);
-//$data = curl_exec($handle);
-//curl_close($handle);
-//echo $data;
-error_log($url);
+$handle = curl_init();
+curl_setopt($handle, CURLOPT_URL, $url);
+$data = curl_exec($handle);
+error_log(curl_error($handle);
+curl_close($handle);
+echo $data;
 ?>
